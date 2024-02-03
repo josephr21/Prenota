9822836593af8463f5c914eb0083104a304e32e6
Joseph Ricci <116790914+josephr21@users.noreply.github.com>
2024-02-03 09:35:16 +0100

    Add files via upload

---
+++ prenota/Models/Utente.cs
@@ -0,0 +1,14 @@
+namespace prenota5H.Models;
+
+public class Utente
+{
+    public string? Nome { get; set; }
+
+    public string? Cognome { get; set; }
+
+    public string? Tipo { get; set; }
+    
+    public string? Email { get; set; }
+
+    
+}
